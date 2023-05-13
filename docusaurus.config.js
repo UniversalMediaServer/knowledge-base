065b43400aff85d7f746cf70e9f1db1ef6ca8906
Klaus Townsend <subjunk@gmail.com>
2023-05-14 09:50:12 +1200

    Use docs-only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,46 +40,20 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/UniversalMediaServer/knowledge-base/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false, // Optional: disable the blog plugin
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
   ],
-
-//     <nav class="nav-menu d-none d-lg-block">
-//       <ul>
-//         <li><a href="/#hero">Home</a></li>
-//         <li><a href="/#features">Features</a></li>
-//         <li><a href="/#about">About</a></li>
-//         <li><a href="/#news">News</a></li>
-//         <li class="drop-down">
-//           <a href="">Support</a>
-//           <ul>
-//             <li><a href="https://support.universalmediaserver.com"><i class='bx bx-book'></i> Knowledge base</a></li>
-//             <li><a href="/forum/"><i class='bx bx-support'></i> Forum</a></li>
-//             <li><a href="https://github.com/UniversalMediaServer/UniversalMediaServer/issues"><i class='bx bx-bug'></i> Bug report</a></li>
-//           </ul>
-//         </li>
-//         <li class="drop-down">
-//           <a href="">Contribute</a>
-//           <ul>
-//             <li><a href="https://github.com/UniversalMediaServer/UniversalMediaServer/"><i class='bx bxl-github'></i> Code</a></li>
-//             <li><a href="https://crowdin.com/project/universalmediaserver"><i class='bx bx-world'></i> Translate</a></li>
-//             <li><a href="/donate/"><i class='bx bx-donate-heart'></i> Donate</a></li>
-//           </ul>
-//         </li>
-//         <li><a href="/donate/">Donate</a></li>
-//       </ul>
-//     </nav>
-
-//     <a href="/download/" class="get-started-btn scrollto">Download</a>
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -93,21 +67,15 @@ const config = {
         },
         items: [
           {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'right',
             label: 'To main site',
             href: 'https://www.universalmediaserver.com',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
-          },
-          {
-            type: 'localeDropdown',
-            position: 'right',
           },
         ],
       },
