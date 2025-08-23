8aad1b4de04287b6d6cf4b0ce28bd4bf52e9207b
Klaus Townsend <subjunk@gmail.com>
2025-08-24 09:33:51 +1200

    Release v15

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,7 +60,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/UniversalMediaServer/knowledge-base/edit/main',
-          lastVersion: '14.x',
+          lastVersion: 'current',
           versions: {
             current: {
               label: '15.x',
